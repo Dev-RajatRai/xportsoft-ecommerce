644ab2f28cb5df7dc2e48917e2ebba0ed6c86bfb
Rajat sood <rajatsood.xportsoft@gmail.com>
2023-05-25 18:50:59 +0530

    complete navbar Design

--- src/Layout/Navbar/index.jsx
+++ src/Layout/Navbar/index.jsx
@@ -11,40 +11,40 @@ const Navbar = () => {
                 {/*     TOP NAVBAR       */}
                 <div className="d-grid top-nav">
 
-                    <div className="logo">
+                    <div className="logo cp">
                         <img src={logo} alt="XPORTSOFT" />
                     </div>
 
                     <div className="location">
-                        <span className="location">Haryana, India</span><br />
+                        <span className="location cp">Haryana, India</span><br />
                         <sub>Free Delivery Available.</sub>
                     </div>
 
                     <div className="searchBar d-flex">
-                        <span>All </span>
+                        <span className="cp">All </span>
                         <input type="search" placeholder="search" />
                         <button type="submit" className="btn search"></button>
                     </div>
 
-                    <div className="language-slector j-center">
-                        <span className="lang">EN</span>
-                        <div className="icon"></div>
+                    <div className="language-selector cp d-flex d-center">
+                        <span className="lang ">EN</span>
+                        <div className="icon">&#9662;</div>
                     </div>
 
-                    <div className="cart j-center d-flex">
+                    <div className="cart d-center d-flex">
                         <div className="icon">
-                            <img src={cart} alt="user" />
+                            <img src={cart} alt="user cp" />
                         </div>
-                        <span className="cart-unit">0</span>
+                        <span className="cart-unit cp">0</span>
                     </div>
 
-                    <div className="user j-center">
+                    <div className="user d-center">
 
                         <div className="user-details d-flex">
                             <div className="icon">
                                 <img src={user} alt="user" />
                             </div>
-                            <p className="details">Hello, user</p>
+                            <p className="details cp">Hello, user</p>
                         </div>
 
                     </div>
@@ -53,7 +53,7 @@ const Navbar = () => {
 
                 <div className=" bottom-nav d-grid">
 
-                    <div className="menu">
+                    <div className="menu cp">
                         <span></span>
                         <span></span>
                         <span></span>
@@ -63,25 +63,25 @@ const Navbar = () => {
 
                         <ul>
 
-                            <li className="nav-link">Apparels</li>
+                            <li className="nav-link cp">Apparels</li>
 
-                            <li className="nav-link">Men's</li>
+                            <li className="nav-link cp">Men's</li>
 
-                            <li className="nav-link">Women's</li>
+                            <li className="nav-link cp">Women's</li>
 
-                            <li className="nav-link">Gadgets</li>
+                            <li className="nav-link cp">Gadgets</li>
 
-                            <li className="nav-link">Electronics</li>
+                            <li className="nav-link cp">Electronics</li>
 
-                            <li className="nav-link">Kitchen</li>
+                            <li className="nav-link cp">Kitchen</li>
 
-                            <li className="nav-link">Decoration</li>
+                            <li className="nav-link cp">Decoration</li>
 
-                            <li className="nav-link">Kids</li>
+                            <li className="nav-link cp">Kids</li>
 
-                            <li className="nav-link">Foot Wear</li>
+                            <li className="nav-link cp">Foot Wear</li>
 
-                            <li className="nav-link">Top Deals</li>
+                            <li className="nav-link cp">Top Deals</li>
 
                         </ul>
                     </div>
